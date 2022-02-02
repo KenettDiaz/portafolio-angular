7ad6c10172d5c9067c5e5355266a879a51630ae9
kenett <kenettandresdiaztaype@gmail.com>
2022-02-02 14:12:09 -0500

    informacion en about desde firebase corriendo

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,16 +10,38 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor(private http: HttpClient) { 
 
     console.log('Servicio de infoPagina Listo');
-    //leer archivos json file
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-    this.http.get('assets/data/data-pagina.json')
-    .subscribe( (resp: InfoPagina) =>{
-      this.cargada = true; 
-      this.info = resp;
+  private cargarInfo(){
+     //leer archivos json file
+
+     this.http.get('assets/data/data-pagina.json')
+     .subscribe( (resp: InfoPagina) =>{
+       this.cargada = true; 
+       this.info = resp;
+       console.log( resp );
+     });
+  }
+
+  private cargarEquipo(){
+    //leer archivos json de firebase
+
+    this.http.get('https://angular-html-b2535-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe( (resp: any) =>{
+      
+      this.equipo = resp;
       console.log( resp );
     });
+    
+    
+    //this.equipo = resp
   }
+
 }
